ead62ea87755b3d6fd1f05b362e1a76913add579
Noel Anstey <anstey.ng@gmail.com>
2017-04-14 13:20:13 -0700

    migrated and seeded db for reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -132,5 +132,37 @@ cat3.products.create!({
   price: 2_483.75
 })
 
+# Users
+
+# puts "Recreating users ..."
+
+# User.destroy_all
+
+# User.create!({
+#   first_name: Noel,
+#   last_name: Anstey,
+#   email: anstey.ng@gmail.com
+#   password_digest: ????
+#   })
+
+# Reviews
+
+puts "Recreating reviews ..."
+
+Review.destroy_all
+
+Review.create!({
+  product_id: 1,
+  user_id: 1,
+  description: "Great product, would buy again!",
+  rating: 5
+})
+
+Review.create!({
+  product_id: 2,
+  user_id: 1,
+  description: "Terrible product, would NOT recommend!",
+  rating: 1
+})
 
 puts "DONE!"
