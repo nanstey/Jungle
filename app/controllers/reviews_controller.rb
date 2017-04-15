4a85723f46da1688b7d4a597b7ee35ac4748b69b
Noel Anstey <anstey.ng@gmail.com>
2017-04-14 17:47:53 -0700

    implemented review submission with some styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,25 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise "Yay, I'm here!"
+    review = Review.new(review_params)
+    review.user_id = session[:user_id]
+    review.product_id = params[:product_id]
+    review.save
+    redirect_to product_path(params[:product_id])
   end
 
   def review_params
     params.require(:review).permit(:rating, :description)
   end
+
+  def product_id
+    params.permit(:product_id)
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+
 
 end
