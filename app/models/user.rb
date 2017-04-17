b01b7b647a227eed35ccb934bf4a13c076b3103c
Noel Anstey <anstey.ng@gmail.com>
2017-04-17 14:29:14 -0700

    validation for duplicated emails case-insensitivity, validation for passwords shorter than 6 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :first_name, :last_name, :email, :password_digest, :password, :password_confirmation, presence: true
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, case_sensitive: false
+  validates :password, length: {minimum: 6}
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'Validations' do
+  describe 'Validations:' do
     it 'should be valid with all fields present' do
-      user = User.new(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'pizza', password_confirmation: 'pizza')
+      user = User.create(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'pizzapie', password_confirmation: 'pizzapie')
       expect(user).to be_valid
     end
     it 'should not be valid with mismatched passwords' do
-      user = User.new(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'pizza', password_confirmation: 'taco')
+      user = User.create(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'pizzapie', password_confirmation: 'tacobell')
       expect(user).to_not be_valid
     end
     it 'should not be valid without password' do
-      user = User.new(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: nil, password_confirmation: 'taco')
+      user = User.create(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: nil, password_confirmation: 'tacobell')
       expect(user).to_not be_valid
     end
     it 'should not be valid without password confirmation' do
-      user = User.new(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'pizza', password_confirmation: nil)
+      user = User.create(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'pizzapie', password_confirmation: nil)
       expect(user).to_not be_valid
     end
     it 'should not be valid to reuse an email' do
-      User.new(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'pizza', password_confirmation: 'pizza')
-      user = User.new(first_name: 'John', last_name: 'Doe', email: 'john@DOE.com', password: 'taco', password_confirmation: 'taco')
+      User.create(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'pizzapie', password_confirmation: 'pizzapie')
+      user = User.create(first_name: 'John', last_name: 'Doe', email: 'john@DOE.com', password: 'tacobell', password_confirmation: 'tacobell')
+      expect(user).to_not be_valid
+    end
+    it 'should not be valid to use a password shorter than 6 characters' do
+      user = User.create(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', password: 'pizza', password_confirmation: 'pizza')
       expect(user).to_not be_valid
     end
   end
