83d4a3c8a4cec402cdcff09e22f53f1ad48bb5d3
Noel Anstey <anstey.ng@gmail.com>
2017-04-17 12:37:02 -0700

    validation tests for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 
-  monetize :price_cents, numericality: true
+  monetize :price_cents, numericality: true, allow_nil: true
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
